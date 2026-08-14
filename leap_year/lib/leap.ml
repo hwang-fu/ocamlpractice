ed4467740ba70a21e9069c8b7b4f0912352ef883
hwangfucius <junzhe.hwangfu@gmail.com>
2026-08-14 16:10:47 +0200

    chore(leap-year): scaffold dune project skeleton

---
+++ leap_year/lib/leap.ml
@@ -0,0 +1,2 @@
+(* [is_leap year] returns [true] iff [year] is a leap year. *)
+let is_leap _year = failwith "TODO: to be filled in after our discussion"
---
+++ leap_year/test/test_leap.ml
@@ -0,0 +1,2 @@
+(* Tests will be added once [Leap.is_leap] is implemented. *)
+let () = ()
