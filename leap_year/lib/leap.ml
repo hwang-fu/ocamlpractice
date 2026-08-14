128de0a9d64d2f4c3a9c50fbcfd71f0a5d666042
hwangfucius <junzhe.hwangfu@gmail.com>
2026-08-14 16:25:22 +0200

    feat(leap-year): implement is_leap and CLI driver

--- leap_year/lib/leap.ml
+++ leap_year/lib/leap.ml
@@ -1,2 +1,3 @@
-(* [is_leap year] returns [true] iff [year] is a leap year. *)
-let is_leap _year = failwith "TODO: to be filled in after our discussion"
+(* [is_leap year] returns [true] iff [year] is a leap year in the Gregorian
+   calendar: divisible by 4 and not by 100, or divisible by 400. *)
+let is_leap year = (year mod 4 = 0 && year mod 100 <> 0) || year mod 400 = 0
